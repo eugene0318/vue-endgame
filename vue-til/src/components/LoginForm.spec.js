ce989b47b8d7289d3786ae93b7c089e8a651771a
eugene0318 <nttott@gmail.com>
2023-06-23 01:23:03 +0900

    final

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -4,7 +4,17 @@ import { shallowMount } from "@vue/test-utils";
 
 describe("LoginFrom.vue", () => {
   test("컴포넌트가 마운팅되면 화면에 그려져야 한다.", () => {
-    const wrapper = shallowMount(LoginFrom);
-    expect(wrapper.vm.username).toBe("");
+    const wrapper = shallowMount(LoginFrom, {
+      data() {
+        return {
+          username: "test",
+        };
+      },
+    });
+    const idInput = wrapper.find("#username");
+    console.log(idInput.element.value);
+    console.log(wrapper.vm.isUsernameValid);
   });
+
+  test("id, pw입력되지 않으면 로그인 버튼이 비활성화");
 });
